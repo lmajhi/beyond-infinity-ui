5161a37d9feb0f923a2245c39d609cf397e3d088
Sushmita <sushmitakumari363@gmail.com>
2024-02-27 12:07:11 +0530

    Initial commit

---
+++ src/common/TopHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TopHeader = () => {
+  return (
+    <div
+      style={{
+        height: 70,
+        backgroundColor: "grey",
+      }}
+    >
+      Header
+    </div>
+  );
+};
+
+export default TopHeader;
---
+++ src/pages/Hompage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+import TopHeader from "../common/TopHeader";
+
+const HomePage = () => {
+  return (
+    <>
+      <TopHeader />
+
+      <Text fontSize="6xl">Homepage </Text>
+    </>
+  );
+};
+
+export default HomePage;
