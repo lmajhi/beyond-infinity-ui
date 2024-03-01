f5134b3c2ca4142e34224cc873c3d46bb2ec58e2
Laxminarayan Majhi <laxminarayanmajhi@Laxminarayans-Mac-mini.local>
2024-03-01 07:16:17 +0530

    Added Typewriter component for displaying search text.

--- src/pages/ResultList.jsx
+++ src/pages/ResultList.jsx
@@ -1,11 +1,13 @@
 import { Box, List, ListItem } from "@chakra-ui/react";
 import React from "react";
 import ItemCard from "./ItemCard";
+import Typewriter from "../common/Typwriter";
 
 const ResultList = ({ response }) => {
   console.log("resultLiust", response);
   return (
     <Box mt={20} minWidth={"90%"} bg={"#f9fafc"}>
+      <Typewriter text={response.search_summary || ""} delay={20} />
       <ItemCard responseData={response} />
     </Box>
   );
